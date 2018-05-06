ab7dba884f6c34b077f9e5d47d0dd383809044f9
gsornsen <gsornsen@gmail.com>
2018-05-06 13:54:53 -0700

    added apt for mopidy and ncmpcpp as they will not be on the distro

--- setEnvironment.sh
+++ setEnvironment.sh
@@ -20,9 +20,14 @@ cp -r $gitDir/Xresources/.Xdefault ~/.Xdefault
 cp -r $gitDir/config/neofetch ~/.config/neofetch
 
 # ncmpcpp
+sudo apt install ncmpcpp
 mkdir -p ~/.ncmpcpp
 cp -r $gitDir/ncmpcpp/config ~/.ncmpcpp/config
 
 # mopidy
+wget -q -O  —  https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
+sudo apt update;sudo apt install mopidy mopidy-spotify
+# Configure mopidy
 cp -r $gitDir/mopidy/org.gnome.mopidyfifo.desktop ~/.config/autostart
 sudo cp -r $gitDir/mopidy/mopidy.conf /etc/mopidy/mopidy.conf
