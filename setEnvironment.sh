c0484b12bf87afd449579fe419d6b426fecc8617
gsornsen <gsornsen@gmail.com>
2018-05-06 14:11:55 -0700

    Install neofetch

--- setEnvironment.sh
+++ setEnvironment.sh
@@ -17,6 +17,7 @@ cp -r $gitDir/Xresources/.Xresources ~/.Xresources
 cp -r $gitDir/Xresources/.Xdefault ~/.Xdefault
 
 # neofetch
+sudo apt install neofetch
 cp -r $gitDir/config/neofetch ~/.config/neofetch
 
 # ncmpcpp
@@ -27,7 +28,7 @@ cp -r $gitDir/ncmpcpp/config ~/.ncmpcpp/config
 # mopidy
 wget -q -O  —  https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
-sudo apt update;sudo apt install mopidy mopidy-spotify
+sudo apt install mopidy mopidy-spotify
 # Configure mopidy
 cp -r $gitDir/mopidy/org.gnome.mopidyfifo.desktop ~/.config/autostart
 sudo cp -r $gitDir/mopidy/mopidy.conf /etc/mopidy/mopidy.conf
